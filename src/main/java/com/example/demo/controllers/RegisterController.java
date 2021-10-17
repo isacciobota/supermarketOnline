526a9c8398188709e2fb254a827408b3052a0e54
Emanuel Bancila <bancila.emanuel953@gmail.com>
2021-10-17 14:57:24 +0300

    register and login backend

--- src/main/java/com/example/demo/controllers/RegisterController.java
+++ src/main/java/com/example/demo/controllers/RegisterController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controllers;
 
+import com.example.demo.exceptions.AccountExists;
 import com.example.demo.exceptions.UsernameAlreadyExistsException;
 import com.example.demo.model.User;
 import com.example.demo.services.UserService;
@@ -10,22 +11,39 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class RegisterController {
 
-    @GetMapping("/register")
+    @GetMapping("/")
     public String greetingForm(Model model) {
         model.addAttribute("user", new User());
-        return "register";
+        return "index";
     }
 
     @PostMapping("/register")
-    public String greetingSubmit(@ModelAttribute User user, Model model) throws UsernameAlreadyExistsException {
+    public String greetingSubmit(@ModelAttribute User user, Model model) {
         model.addAttribute("user", user);
         try {
+            user.setRole("client");
             UserService.addUser(user.getUsername(), user.getPassword(), user.getRole(), user.getName(), user.getAddress(), user.getEmail());
         }
         catch (UsernameAlreadyExistsException e)
         {
-            return "register2";
+            return "index";
         }
-        return "rez";
+        return "index";
+    }
+    @PostMapping("/login")
+    public String login(@ModelAttribute User user, Model model){
+        model.addAttribute("user",user);
+        try{
+            UserService.checkUsernameAndPassword(user.getUsername(),user.getPassword());
+        }
+        catch(AccountExists e)
+        {
+            if(UserService.getUserRole(user.getUsername(),user.getPassword()).equals("client"))
+            {
+                return "clientHome";
+            }
+            return "adminHome";
+        }
+        return "index";
     }
 }
